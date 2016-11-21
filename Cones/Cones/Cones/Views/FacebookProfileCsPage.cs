df21035680ab6e72318f1a020c39f518d682fbd6
AdityaJNair <adijnair@gmail.com>
2016-11-22 00:41:03 +1300

    working Cones FB

---
+++ Cones/Cones/Cones/Views/FacebookProfileCsPage.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookLogin.Models;
+using FacebookLogin.ViewModels;
+using Xamarin.Forms;
+using Device = Xamarin.Forms.Device;
+using System.Diagnostics;
+
+namespace FacebookLogin.Views
+{
+    public class FacebookProfileCsPage : ContentPage
+    {
+
+        /// <summary>
+        /// Make sure to get a new ClientId from:
+        /// https://developers.facebook.com/apps/
+        /// </summary>
+        private string ClientId = "1693968570915552";
+
+        public FacebookProfileCsPage()
+        {
+
+            BindingContext = new FacebookViewModel();
+            BackgroundColor = Color.White;
+
+            var apiRequest =
+                "https://www.facebook.com/dialog/oauth?client_id="
+                + ClientId
+                + "&display=popup&response_type=token&redirect_uri=https://www.facebook.com/connect/login_success.html";
+
+            var webView = new WebView
+            {
+                Source = apiRequest,
+                HeightRequest = 1
+            };
+
+            webView.Navigated += WebViewOnNavigated;
+
+            Content = webView;
+        }
+
+        private async void WebViewOnNavigated(object sender, WebNavigatedEventArgs e)
+        {
+
+            var accessToken = ExtractAccessTokenFromUrl(e.Url);
+
+            if (accessToken != "")
+            {
+                try
+                {
+                    SetPageFail();
+                    var vm = BindingContext as FacebookViewModel;
+
+                    await vm.SetFacebookUserProfileAsync(accessToken);
+
+                    SetPageContent(vm.FacebookProfile);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                }
+
+
+            }
+        }
+
+        private void SetPageFail()
+        {
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "hello",
+                        TextColor = Color.Black,
+                        FontSize = 50,
+                    }
+                }
+            };
+         }
+
+
+        private void SetPageContent(FacebookProfile facebookProfile)
+        {
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Padding = new Thickness(8, 30),
+                Children =
+                {
+                    new Label
+                    {
+                        Text = facebookProfile.Name,
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.Id,
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.IsVerified.ToString(),
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.Devices.FirstOrDefault().Os,
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.Gender,
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.AgeRange.Min.ToString(),
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.Picture.Data.Url,
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                    new Label
+                    {
+                        Text = facebookProfile.Cover.Source,
+                        TextColor = Color.Black,
+                        FontSize = 22,
+                    },
+                }
+            };
+        }
+
+        /// <summary>
+        /// Getting the access token
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private string ExtractAccessTokenFromUrl(string url)
+        {
+            if (url.Contains("access_token") && url.Contains("&expires_in="))
+            {
+                var at = url.Replace("https://www.facebook.com/connect/login_success.html#access_token=", "");
+
+                if (Device.OS == TargetPlatform.WinPhone || Device.OS == TargetPlatform.Windows)
+                {
+                    at = url.Replace("http://www.facebook.com/connect/login_success.html#access_token=", "");
+                }
+
+                var accessToken = at.Remove(at.IndexOf("&expires_in="));
+
+                return accessToken;
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Cones/Cones/Cones/Views/MainCsPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FacebookLogin.Views
+{
+    public class MainCsPage : ContentPage
+    {
+
+        public MainCsPage()
+        {
+            BackgroundImage = "cones.png";
+
+            var loginButton = new Button
+            {
+                Text = "Login with Facebook",
+                TextColor = Color.White,
+                BackgroundColor = Color.FromHex("#01579B"),
+            };
+
+            loginButton.Clicked += LoginWithFacebook_Clicked;
+
+            Content = new StackLayout
+            {
+                Spacing = 0,
+                Padding = 10,
+                VerticalOptions = LayoutOptions.End,
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    loginButton
+                }
+            };
+        }
+
+        private async void LoginWithFacebook_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FacebookProfileCsPage());
+        }
+    }
+}
